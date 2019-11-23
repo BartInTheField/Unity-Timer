7fdb69bab7dced66336fe584ad0a179094d6e5c8
Bart in t Veld <bart.int.007@gmail.com>
2019-11-23 12:03:56 +0200

    Init

---
+++ Assets/BartInTheField/BartInTheField.Timer/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BartInTheField.Timer
+{
+    public class Timer : MonoBehaviour
+    {
+        public event Action OnTimeout = delegate { };
+        public event Action<float> OnTimeChanged = delegate { };
+
+        [SerializeField] private float startTime = 1f;
+        [SerializeField] private bool loop = false;
+        [SerializeField] private bool autoStart = true;
+
+        private bool timeout = true;
+        private float timer = 0.0f;
+
+        private void Start()
+        {
+            if (autoStart)
+            {
+                StartTimer();
+            }
+        }
+
+        private void Update()
+        {
+            if (timer > 0 && !timeout)
+            {
+                timer -= Time.deltaTime;
+                OnTimeChanged.Invoke(timer);
+            }
+            else if (timer < 0 && !timeout)
+            {
+                timeout = true;
+                OnTimeout.Invoke();
+
+                if (loop)
+                {
+                    StartTimer();
+                }
+            }
+        }
+
+        public void StartTimer()
+        {
+            timer = startTime;
+            timeout = false;
+        }
+    }
+}
---
+++ Assets/TestingScript/TimerTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BartInTheField.Timer;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerTester : MonoBehaviour
+{
+    [SerializeField] private Timer timer = default;
+
+    [SerializeField] private Text counterText = default;
+
+    private void Start()
+    {
+        timer.OnTimeout += ReactToTimeout;
+        timer.OnTimeChanged += SetCounterText;
+    }
+
+    private void OnDestroy()
+    {
+        timer.OnTimeout -= ReactToTimeout;
+        timer.OnTimeChanged -= SetCounterText;
+    }
+
+    private void ReactToTimeout()
+    {
+        print("Timeout");
+    }
+
+    private void SetCounterText(float counter)
+    {
+        counterText.text = Mathf.Ceil(counter) + "";
+    }
+}
